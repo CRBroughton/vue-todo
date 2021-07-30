316435a656e3feef9745d6f05689cc6862e08b66
CRBroughton <crbroughton@posteo.uk>
2021-07-30 15:00:43 +0100

    TodoList now fetches todos from localbase

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,29 @@
 <template>
-  <div v-for="todo in todos" :key="todo.id">
+  <div v-for="todo in todos" :key="todo.id" class="bg-gray-100 text-center">
     <h1>{{ todo.contents }}</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import { onMounted } from "@vue/runtime-core";
 import Todo from "@/types/Todo";
+import db from "@/localbase";
 
 export default defineComponent({
   setup() {
+    onMounted(() => {
+      getTodos();
+    });
+
     const todos = ref<Todo[]>([]);
+    const getTodos = async () => {
+      todos.value = await db.collection("todo").get();
+    };
+
     return {
       todos,
+      getTodos,
     };
   },
 });
