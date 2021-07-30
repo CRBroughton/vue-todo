f0c1ab9f4836213328f6b06c5ecaa262970c6b89
CRBroughton <crbroughton@posteo.uk>
2021-07-30 12:13:13 +0100

    added svg change when SideMenu is toggled

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -2,6 +2,7 @@
   <CreateTodo v-if="CreateTodoToggle" />
   <div class="absolute bottom-0 right-0 m-3 cursor-pointer">
     <svg
+      v-if="CreateTodoToggle === false"
       class="w-14 h-14 bg-gray-200 rounded-full text-gray-400"
       fill="none"
       stroke="currentColor"
@@ -14,6 +15,21 @@
         stroke-linejoin="round"
         stroke-width="2"
         d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      ></path>
+    </svg>
+    <svg v-else
+      class="w-14 h-14 bg-gray-200 rounded-full text-gray-400"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      @click.prevent="CreateTodo"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M20 12H4"
       ></path>
     </svg>
   </div>
