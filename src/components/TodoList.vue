20adff9ba1838e35a303439b89a542f3b35b0c69
CRBroughton <crbroughton@posteo.uk>
2021-08-02 11:55:08 +0100

    moved delete button out of li

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,27 +1,39 @@
 <template>
   <div class="text-center h-screen pt-1">
-    <ul v-for="todo in todos" :key="todo.id" class="p-1">
+    <ul v-for="todo in todos" :key="todo.id" class="p-1 flex items-center">
       <li
-        @click="setTodoToComplete(todo.id)"
-        class="my-1 p-4 rounded-lg bg-gray-100 items-center flex break-all"
+        @click.prevent="setTodoToComplete(todo.id)"
+        class="
+          my-1
+          p-4
+          w-full
+          rounded-lg
+          bg-gray-100
+          items-center
+          flex
+          break-all
+        "
         :class="todo.isCompleted && lineThrough"
       >
         {{ todo.contents }}
-        <div class="absolute right-0 mr-6" @click="DeleteTodoFromList(todo.id)">
-          <svg
-            class="w-6 h-6 text-gray-600"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </div>
       </li>
+      <div
+        class="bg-red-200 right-0 ml-2 w-14 h-14 rounded-lg flex items-center"
+        @click.prevent="DeleteTodoFromList(todo.id)"
+      >
+        <svg
+          class="w-8 h-8 text-gray-600 m-auto"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </div>
     </ul>
   </div>
 </template>
